96113821e43ec34a7c49ce615a4dc465089dc368
Victor Andrey <109192128+vandreys@users.noreply.github.com>
2022-12-11 22:23:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+window.sr = ScrollReveal({ reset:true});
+
+sr.reveal('.area-1', {
+    rotate: {x:0, y: 80, z:0},
+    duration: 1500
+});
+
+sr.reveal('.area-2', {
+    rotate: {x:0, y: 80, z:0},
+    duration: 1500
+});
+
+sr.reveal('.area-3', {
+    rotate: {x:0, y: 80, z:0},
+    duration: 1500
+});
